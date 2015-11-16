43ec2b418799977d1a0ee23739b94d31a8772b89
David <rulateunmai@hotmail.com>
2015-11-16 10:54:33 +0100

    Creada la clase ClockDisplay

---
+++ ClockDisplay.java
@@ -0,0 +1,17 @@
+
+/**
+ * * Se crea un reloj que marca la hora y los segundos.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    //La hora del reloj.
+    private NumberDisplay horas;
+    //Los minutos del reloj.
+    private NumberDisplay minutos;
+    //La hora y los minutos actuales en 5 caracteres.
+    private String horaActual;
+    
+}
