00b64f486576f7198cba4e3281ab51150f961755
David <rulateunmai@hotmail.com>
2015-11-16 11:37:46 +0100

    Creados dos constructores: uno sin parámetros, que fija la hora a 00:00 y otro con 2 parámetros de tipo int para fijar la hora actual.

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -14,4 +14,27 @@ public class ClockDisplay
     //La hora y los minutos actuales en 5 caracteres.
     private String horaActual;
     
+    /**
+     * Crea un reloj cuya hora de inicio es 00:00.
+     */
+    public ClockDisplay()
+    {
+        //Inicializa el reloj a 00:00.
+        horas = new NumberDisplay(24);        
+        minutos = new NumberDisplay(60);
+        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue();        
+    }
+
+    /**
+     * Crea un reloj cuya hora de inicio se fija al crearlo.
+     */
+    public ClockDisplay(int h, int m)
+    {
+        //Inicializa el reloj a una hora fijada en horas y miutos.
+        horas = new NumberDisplay(24);
+        horas.setValue(h);
+        minutos = new NumberDisplay(60);
+        minutos.setValue(m);
+        horaActual = horas.getDisplayValue() + ":" + minutos.getDisplayValue(); 
+    }   
 }
